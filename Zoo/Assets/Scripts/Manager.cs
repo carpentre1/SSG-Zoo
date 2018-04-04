59ac77fdd2db0ae57adf5ddf36903182b8625895
carpentre1 <jlowell.carpenter@gmail.com>
2018-04-03 22:02:02 -0500

    Imported assets, initial scene setup

---
+++ Zoo/Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void NextMonth()//when they click the "next month" button,
+    {
+        //make sure they're ready to start the next month
+        //calculate events
+        //show visitors walking around
+        //show animals doing cute stuff
+        //end all the action
+        //give them new decisions to make
+    }
+}
