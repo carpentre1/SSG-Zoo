4638024831a1d5c7af47c3ff51d914b3a233d553
carpentre1 <jlowell.carpenter@gmail.com>
2018-04-27 18:18:12 -0500

    Animal Behavior

--- Zoo/Assets/Scripts/Manager.cs
+++ Zoo/Assets/Scripts/Manager.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour {
 
+
+    public float money = 0;
+    public List<GameObject> allAnimals = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
-		
+        money += 200000;//starting funds
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        AnimalBehavior();
 	}
 
     void NextMonth()//when they click the "next month" button,
@@ -22,5 +25,20 @@ public class Manager : MonoBehaviour {
         //show animals doing cute stuff
         //end all the action
         //give them new decisions to make
+    }
+
+    void AnimalBehavior()
+    {
+        if(allAnimals.Count == 0) { return; }
+        foreach (GameObject animal in allAnimals)
+        {
+            Rigidbody2D rb = animal.GetComponent<Rigidbody2D>();
+            if(Random.value > .995f)
+            {
+                rb.velocity = new Vector2(0, 0);
+                Vector2 moveDirection = new Vector2(Random.Range(-1,2), Random.Range(-1,2));
+                rb.AddForce(moveDirection * (Random.Range(1,50)));
+            }
+        }
     }
 }
---
+++ Zoo/Assets/Scripts/UI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+    public GameObject Manager;
+    Manager managerScript;
+
+    public GameObject penguin;
+    public GameObject snake;
+    public GameObject zebra;
+    public GameObject gorilla;
+    public GameObject elephant;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        managerScript = Manager.GetComponent<Manager>();
+	}
+
+    public void BuyAnimal(string animalType)
+    {
+        if(animalType == "Penguin")
+        {
+            AttemptPurchase(penguin, 1000);
+        }
+        if (animalType == "Snake")
+        {
+            AttemptPurchase(snake, 1000);
+        }
+        if (animalType == "Zebra")
+        {
+            AttemptPurchase(zebra, 1000);
+        }
+        if (animalType == "Elephant")
+        {
+            AttemptPurchase(elephant, 1000);
+        }
+        if (animalType == "Gorilla")
+        {
+            AttemptPurchase(gorilla, 1000);
+        }
+    }
+
+    void AttemptPurchase(GameObject animal, float cost)
+    {
+        if(animal.name.Contains("Owned")) { return; }
+        if(managerScript.money < cost) { return; }
+        else
+        {
+            GameObject animal1 = Instantiate(animal, new Vector3(animal.transform.position.x + Random.value, animal.transform.position.y + Random.value, animal.transform.position.z), Quaternion.identity);
+            GameObject animal2 = Instantiate(animal, new Vector3(animal.transform.position.x + Random.value, animal.transform.position.y + Random.value, animal.transform.position.z), Quaternion.identity);
+            GameObject animal3 = Instantiate(animal, new Vector3(animal.transform.position.x + Random.value, animal.transform.position.y + Random.value, animal.transform.position.z), Quaternion.identity);
+            GameObject animal4 = Instantiate(animal, new Vector3(animal.transform.position.x + Random.value, animal.transform.position.y + Random.value, animal.transform.position.z), Quaternion.identity);
+            animal.SetActive(true); animal2.SetActive(true); animal3.SetActive(true); animal4.SetActive(true);
+            managerScript.allAnimals.Add(animal1); managerScript.allAnimals.Add(animal2); managerScript.allAnimals.Add(animal3); managerScript.allAnimals.Add(animal4);
+            animal.name += "Owned";
+            managerScript.money -= cost;
+            Debug.Log(penguin.transform.position);
+        }
+    }
+}
