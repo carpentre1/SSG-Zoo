089aa9b43d802cc244b7a4041c6f374ab729cb3d
carpentre1 <jlowell.carpenter@gmail.com>
2018-05-08 00:06:41 -0500

    Build

--- Zoo/Assets/Scripts/Manager.cs
+++ Zoo/Assets/Scripts/Manager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Manager : MonoBehaviour {
-
+    //playtest results:
+    //100 happiness route: 12k funds left, 6.6k profit total
+    //profit route (not optimal): 23k funds left, 6.3k profit, 50 happiness
     public Button nextMonthButton;
+    public Text nextMonthButtonText;
+
+    public Canvas mainCanvas;
     public UI UIScript;
     public GameObject hiderPanel;
+
+    public GameObject endingPanel;
+    public Text statsText;
+    public Text analysisText;
 
     bool touristsVisiting = false;
     
@@ -20,10 +29,17 @@ public class Manager : MonoBehaviour {
     float animalMoveVelocityNightMultiplier = 3f;
     float animalMoveVelocityDayMultiplier = 3f;
 
+    public enum animalProfits { gorilla=800, zebra=850, penguin=400, elephant=900, snake=440}
+    public int gorillasOwned = 0;
+    public int zebrasOwned = 0;
+    public int penguinsOwned = 0;
+    public int elephantsOwned = 0;
+    public int snakesOwned = 0;
+
     public int currentMonth = 1;
     public float money = 0;
     public float happiness = 50;
-    public float happinessPerMonth = 0;
+    public float happinessPerMonth = -5;
 
     public List<GameObject> allAnimals = new List<GameObject>();
 
@@ -33,10 +49,11 @@ public class Manager : MonoBehaviour {
     public float totalProfit = 0;
 	// Use this for initialization
 	void Start () {
-        money += 200000;//starting funds
+
+
+        money += 2000;//starting funds
         currentAnimalMoveFrequencyMultiplier = animalMoveFrequencyDayMultiplier;
         currentAnimalMoveVelocityNightMultiplier = animalMoveVelocityDayMultiplier;
-        UIScript.UpdateUI();
 
     }
 	
@@ -47,20 +64,52 @@ public class Manager : MonoBehaviour {
 
     public void CalculateProfit()
     {
-        totalProfit = (animalProfit * (happiness / 100)) + shopProfit + fundingProfit;
+        float gorillaProfit = ((int)animalProfits.gorilla * happiness / 100 * gorillasOwned);
+        float snakeProfit = ((int)animalProfits.snake * happiness / 100 * snakesOwned);
+        float elephantProfit = ((int)animalProfits.elephant * happiness / 100 * elephantsOwned);
+        float penguinProfit = ((int)animalProfits.penguin * happiness / 100 * penguinsOwned);
+        float zebraProfit = ((int)animalProfits.zebra * happiness / 100 * zebrasOwned);
+        Debug.Log("snake" + snakeProfit + "gorilla" + gorillaProfit + "elephant" + elephantProfit + "penguin" + penguinProfit + "zebra" + zebraProfit);
+        animalProfit = gorillaProfit + snakeProfit + elephantProfit + penguinProfit + zebraProfit;
+        if(happiness < 30) { fundingProfit = 0; }
+        if(happiness > 30) { fundingProfit = 2000; }
+        totalProfit = animalProfit + shopProfit + fundingProfit;
         UIScript.UpdateUI();
     }
 
     public void NextMonth()//when they click the "next month" button,
     {
         //make sure they're eligible to start the next month
-        if(touristsVisiting || currentMonth >= 12)
+        if(touristsVisiting)
         {
             return;
+        }
+        if(currentMonth >= 12)
+        {
+            endingPanel.SetActive(true);
+            statsText.text = "You had $" + money + " of remaining funding, and a total profit of $" + totalProfit + "." +
+                             "\nThe overall happiness of your animals was " + happiness + "/100.";
+            if(happiness > 80)
+            {
+                analysisText.text = "Based on these results, it look like you tried to make your animals as happy as possible. Did you struggle with acquiring all the funds you needed?" +
+                    "\n\nWere you able to buy all the animals you wanted?" + "\n\nHas your outlook on the difficulties of maintaining a successful, happy zoo changed?";
+            }
+            if (happiness > 50 && happiness <= 80)
+            {
+                analysisText.text = "Based on these results, it look like you tried to maintain a balanced approach of acquiring funding and keeping animals happy." +
+                    "\n\nWere you able to buy all the animals you wanted, and make them as happy as you wanted?" + "\n\nHas your outlook on the difficulties of maintaining a successful, happy zoo changed?";
+            }
+            if (happiness <= 50)
+            {
+                analysisText.text = "Based on these results, it look like you tried to make as much money as possible, and weren't concerned about the happiness of your animals." +
+                    "\n\nWere you just trying to see how much money you could possibly make? If you were running a real zoo, would you try to make your animals happier than this?" +
+                    "\n\nHas your outlook on the difficulties of maintaining a successful, happy zoo changed?";
+            }
         }
 
         money += totalProfit;//calculate events
         happiness += happinessPerMonth;
+        if(happiness > 100) { happiness = 100; }
         currentMonth += 1;
         CalculateProfit();
         StartCoroutine(TouristDelay());
@@ -82,13 +131,17 @@ public class Manager : MonoBehaviour {
         hiderPanel.SetActive(true);
         touristsVisiting = true;
         currentAnimalMoveFrequencyMultiplier = animalMoveFrequencyNightMultiplier;
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(1);
         nextMonthButton.gameObject.SetActive(true);
         hiderPanel.SetActive(false);
         touristsVisiting = false;
         UIScript.UpdateChoiceButtons();
         UIScript.ToggleChoiceButtons(true);
         currentAnimalMoveFrequencyMultiplier = animalMoveFrequencyDayMultiplier;
+        if(currentMonth >= 12)
+        {
+            nextMonthButtonText.text = "Results";
+        }
 
     }
 
--- Zoo/Assets/Scripts/UI.cs
+++ Zoo/Assets/Scripts/UI.cs
@@ -8,17 +8,27 @@ public class UI : MonoBehaviour {
     public GameObject Manager;
     Manager managerScript;
 
+    public Button nextMonthButton;
+    public Text tutorialText;
+
     public GameObject penguin;
     public GameObject snake;
     public GameObject zebra;
     public GameObject gorilla;
     public GameObject elephant;
 
+    public Button penguinButton;
+    public Button snakeButton;
+    public Button zebraButton;
+    public Button gorillaButton;
+    public Button elephantButton;
+
     public Text monthText;
     public Text earningsText;
     public Text totalProfitText;
     public Text happinessText;
     public Text generalInfoText;
+    public Text fundsText;
 
     public Button choice1Button;
     public Button choice2Button;
@@ -32,29 +42,42 @@ public class UI : MonoBehaviour {
     string choice2;
     string choice3;
 
-    static string extraFood = "$800: Extra food for all the animals! (instant +5 happiness)";
+    static string extraFood = "$900: Extra food for all the animals! (instant +5 happiness)";
     static string betterAirConditioning = "$1,200: More ideal temperatures for each animal pen (+1 happiness per month)";
-    static string betterAnimalTraining = "$1,500: Hire better trainers for the animals (+1 happiness per month)";
-    static string fancierGiftShop = "$700: Fancier gift shop, attracting more people to buy things from it (+$250 profit)";
+    static string betterAnimalTraining = "$2,000: Hire better trainers for the animals (+1.5 happiness per month)";
+    static string fancierGiftShop = "$600: Fancier gift shop, attracting more people to buy things from it (+$250 profit)";
+    static string moreJanitors = "$2,800: Hire more staff to clean animal pens (+2 happiness per month)";
+    static string stuffedAnimals = "$900: Expand gift shop inventory with stuffed animals (+$600 profit)";
 
     public List<string> choices = new List<string>();
 
     // Use this for initialization
     void Start () {
         choices.Add(extraFood); choices.Add(betterAirConditioning); choices.Add(betterAnimalTraining);
-        choices.Add(fancierGiftShop);
+        choices.Add(fancierGiftShop); choices.Add(moreJanitors); choices.Add(stuffedAnimals);
 
         UpdateChoiceButtons();
+        ToggleChoiceButtons(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         managerScript = Manager.GetComponent<Manager>();
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 	}
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
     public void UpdateUI()
     {
+        Debug.Log("updated ui");
         monthText.text = "Month: " + managerScript.currentMonth;
+        fundsText.text = "Funds: $" + managerScript.money;
         happinessText.text = "Animal happiness: " + managerScript.happiness + "\nHappiness gained per month: " + managerScript.happinessPerMonth;
         earningsText.text = "Expected earnings this month:\nState funding: $" + managerScript.fundingProfit
             + "\nAnimal profits: $" + managerScript.animalProfit
@@ -83,7 +106,7 @@ public class UI : MonoBehaviour {
         float cost;
         if(choice == extraFood)
         {
-            cost = 800;
+            cost = 900;
             if (CanAfford(cost))
             {
                 //do visual stuff
@@ -96,7 +119,7 @@ public class UI : MonoBehaviour {
         }
         if (choice == fancierGiftShop)
         {
-            cost = 700;
+            cost = 600;
             if (CanAfford(cost))
             {
                 //do visual stuff
@@ -106,14 +129,29 @@ public class UI : MonoBehaviour {
                 //hide choice buttons
                 ToggleChoiceButtons(false);
             }
+
         }
-        if (choice == betterAirConditioning)
+        if (choice == stuffedAnimals)
         {
-            cost = 800;
+            cost = 900;
             if (CanAfford(cost))
             {
                 //do visual stuff
-                managerScript.happiness += 5;
+                managerScript.shopProfit += 600;
+                managerScript.money -= cost;
+                managerScript.CalculateProfit();
+                //hide choice buttons
+                ToggleChoiceButtons(false);
+            }
+
+        }
+        if (choice == betterAirConditioning)
+        {
+            cost = 1200;
+            if (CanAfford(cost))
+            {
+                //do visual stuff
+                managerScript.happinessPerMonth += 1;
                 managerScript.money -= cost;
                 managerScript.CalculateProfit();
                 //hide choice buttons
@@ -122,17 +160,31 @@ public class UI : MonoBehaviour {
         }
         if (choice == betterAnimalTraining)
         {
-            cost = 800;
+            cost = 2000;
             if (CanAfford(cost))
             {
                 //do visual stuff
-                managerScript.happiness += 5;
+                managerScript.happinessPerMonth += 1.5f;
                 managerScript.money -= cost;
                 managerScript.CalculateProfit();
                 //hide choice buttons
                 ToggleChoiceButtons(false);
             }
         }
+        if (choice == moreJanitors)
+        {
+            cost = 2800;
+            if (CanAfford(cost))
+            {
+                //do visual stuff
+                managerScript.happinessPerMonth += 2;
+                managerScript.money -= cost;
+                managerScript.CalculateProfit();
+                //hide choice buttons
+                ToggleChoiceButtons(false);
+            }
+        }
+        UpdateUI();
     }
 
     public void ToggleChoiceButtons(bool areVisible)
@@ -172,34 +224,64 @@ public class UI : MonoBehaviour {
         choices.Add(choice1); choices.Add(choice2);
     }
 
+    public void RevealChoices()
+    {
+        nextMonthButton.gameObject.SetActive(true);
+        tutorialText.gameObject.SetActive(false);
+        ToggleChoiceButtons(true);
+    }
+
     public void BuyAnimal(string animalType)
     {
         if(animalType == "Penguin")
         {
-            AttemptPurchase(penguin, 1000);
+            if (AttemptPurchase(penguin, 1000))
+            {
+                penguinButton.gameObject.SetActive(false);
+                managerScript.penguinsOwned += 1;
+                RevealChoices();
+            }
         }
         if (animalType == "Snake")
         {
-            AttemptPurchase(snake, 1000);
+            if (AttemptPurchase(snake, 2000))
+            {
+                snakeButton.gameObject.SetActive(false);
+                managerScript.snakesOwned += 1;
+            }
         }
         if (animalType == "Zebra")
         {
-            AttemptPurchase(zebra, 1000);
+            if (AttemptPurchase(zebra, 7200))
+            {
+                zebraButton.gameObject.SetActive(false);
+                managerScript.zebrasOwned += 1;
+            }
         }
         if (animalType == "Elephant")
         {
-            AttemptPurchase(elephant, 1000);
+            if (AttemptPurchase(elephant, 9600))
+            {
+                elephantButton.gameObject.SetActive(false);
+                managerScript.elephantsOwned += 1;
+            }
         }
         if (animalType == "Gorilla")
         {
-            AttemptPurchase(gorilla, 1000);
+            if (AttemptPurchase(gorilla, 5400))
+            {
+                gorillaButton.gameObject.SetActive(false);
+                managerScript.gorillasOwned += 1;
+            }
         }
+        managerScript.CalculateProfit();
     }
 
-    void AttemptPurchase(GameObject animal, float cost)
+    bool AttemptPurchase(GameObject animal, float cost)
     {
-        if(animal.name.Contains("Owned")) { return; }
-        if(managerScript.money < cost) { return; }
+        Debug.Log("a");
+        if(animal.name.Contains("Owned")) { return false; }
+        if(managerScript.money < cost) { return false; }
         else
         {
             GameObject animal1 = Instantiate(animal, new Vector3(animal.transform.position.x + Random.value, animal.transform.position.y + Random.value, animal.transform.position.z), Quaternion.identity);
@@ -210,8 +292,8 @@ public class UI : MonoBehaviour {
             managerScript.allAnimals.Add(animal1); managerScript.allAnimals.Add(animal2); managerScript.allAnimals.Add(animal3); managerScript.allAnimals.Add(animal4);
             animal.name += "Owned";
             managerScript.money -= cost;
-            Debug.Log(penguin.transform.position);
-            managerScript.CalculateProfit();
+            Debug.Log("bought " + animal.name);
+            return true;
         }
     }
 }
